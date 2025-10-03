fa4bf318a1ce26c2a7ea32aebb73c96541429a6a
KleberMera <klebermera2016@gmail.com>
2025-10-03 17:46:23 -0500

    produccion

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,5 +1,5 @@
 const environments = {
-    apiUrl: 'http://localhost:5000/api',
+    apiUrl: 'https://f4nipzkd60.execute-api.us-west-1.amazonaws.com/production/api',
     apiUrlProduction: 'https://restdeluxe.bingoamigo.net/api'
 };
 // 'https://restdeluxe.bingoamigo.net/api/registradores/dashboard-metrics'
