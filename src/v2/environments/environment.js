a04ecde07e9aec427db50dcc5f7546475f82fcf8
KleberMera <klebermera2016@gmail.com>
2025-10-03 18:08:06 -0500

    produccion

--- src/v2/environments/environment.js
+++ src/v2/environments/environment.js
@@ -1,5 +1,5 @@
 const environments = {
-    apiUrl: 'https://f4nipzkd60.execute-api.us-west-1.amazonaws.com/production/api',
+    apiUrl: 'https://1sow8dpavl.execute-api.us-west-1.amazonaws.com/production/api',
     //apiUrl: 'http://localhost:5000/api',
     apiUrlProduction: 'https://restdeluxe.bingoamigo.net/api'
 };
