31c3eeb503b83468918bc004ad3ac3f74fe3297d
KleberMera <klebermera2016@gmail.com>
2025-10-03 17:49:44 -0500

    .prd

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,5 +1,5 @@
 const environments = {
-    apiUrl: 'https://f4nipzkd60.execute-api.us-west-1.amazonaws.com/production/api',
+    apiUrl: 'http://localhost:5000/api',
     apiUrlProduction: 'https://restdeluxe.bingoamigo.net/api'
 };
 // 'https://restdeluxe.bingoamigo.net/api/registradores/dashboard-metrics'
--- src/v2/environments/environment.js
+++ src/v2/environments/environment.js
@@ -1,6 +1,6 @@
 const environments = {
-    //apiUrl: 'https://f4nipzkd60.execute-api.us-west-1.amazonaws.com/production/api',
-    apiUrl: 'http://localhost:5000/api',
+    apiUrl: 'https://f4nipzkd60.execute-api.us-west-1.amazonaws.com/production/api',
+    //apiUrl: 'http://localhost:5000/api',
     apiUrlProduction: 'https://restdeluxe.bingoamigo.net/api'
 };
 
