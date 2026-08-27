6c1cc2225e8e56119652252efff1addc7f9e6231
KleberMera <klebermera2016@gmail.com>
2026-08-27 09:37:11 -0500

    DEPLOY

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+echo "======================================"
+echo "🚀 Desplegando Frontend BINGO AMIGO..."
+echo "======================================"
+
+echo ""
+echo "📥 Actualizando repositorio..."
+git fetch --all
+git pull
+
+echo ""
+echo "📦 Instalando dependencias..."
+npm install
+
+echo ""
+echo "🏗️ Compilando Bingo Amigo..."
+
+# ng build 
+npm run build
+# ng build --base-href /controlelectoral/
+# ng build --configuration production --base-href /controlelectoral/
+
+echo ""
+echo "📂 Publicando archivos..."
+
+sudo rsync -av --delete /build/ /var/www/bingo/
+
+echo ""
+echo "✅ BINGO AMIGO desplegado correctamente."
+echo "======================================"
